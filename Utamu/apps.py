ef0254076a8d313ece0a4d18f6eca6cc81a26953
veemutuku <vivianmutuku8@gmail.com>
2023-12-01 11:23:49 +0300

    food

---
+++ Utamu/apps.py
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from django.apps import AppConfig
+
+
+class UtamuConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Utamu'
